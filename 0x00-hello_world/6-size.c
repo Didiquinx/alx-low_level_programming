3f07414b6511c0e290e2fa181470473a4a1e202c
Didiquinx <deedeeetim@gmail.com>
2022-12-15 13:50:26 -0800

    Task six

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main -print out sizes of data types in C
+ * main - print out sizes of data types in C
  * code by Didi
  * Return: 0
- */
+*/
 int main(void)
 {
 	char a;
