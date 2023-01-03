137a5c8720c496897efc13be5dcbf91234029dcc
Didiquinx <deedeeetim@gmail.com>
2023-01-03 14:39:33 -0800

    update

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,25 +9,17 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int index;
-
-	if (*needle == 0)
-		return (haystack);
-
-	while (*haystack)
+	for (; *haystack != '\0'; haystack++)
 	{
-		index = 0;
-
-		if (haystack[index] == needle[index])
+		char *l = haystack;
+		char *p = needle;
+		while (*l == *p && *p != '\0')
 		{
-			do {
-				if (needle[index + 1] == '\0')
-					return (haystack);
-
-				index++;
-			} while (haystack[index] == needle[index]);
+			l++;
+			p++;
 		}
-		haystack++;
+		if (*p == '\0')
+			return (haystack);
 	}
 	return (0);
 }
