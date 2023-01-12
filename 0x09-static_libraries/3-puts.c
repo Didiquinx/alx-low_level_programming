017b50305324696e90e49391c1ccca4ede532e07
Didiquinx <deedeeetim@gmail.com>
2023-01-11 19:16:32 -0800

    task 1

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\'programming is like building a multilingual puzzle");
+	return (0);
+}
